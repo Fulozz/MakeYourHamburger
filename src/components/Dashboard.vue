fbc4516fdeec857ecce290ee9e4c1cf02146bb7c
Thiago Silva Andrade <92411755+Fulozz@users.noreply.github.com>
2022-07-05 18:40:24 -0300

    Dashboard Interativo e funcional na solicitação de pedidos

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -11,20 +11,23 @@
             </div>
         </div>
         <div id="burger-table-rows">
-            <div class="burger-table-row">
-                <div class="order-number">1</div>
-                <div>Thiago</div>
-                <div>Pão de Trigo</div>
-                <div>Maminha</div>
+            <div class="burger-table-row" v-for="burger in burgers" :key="burger.id" >
+                <div class="order-number">{{ burger.id }}</div>
+                <div>{{ burger.nome}}</div>
+                <div>{{ burger.pao }}</div>
+                <div>{{ burger.carne }}</div>
                 <div>
                     <ul>
-                        <li>Salame</li>
-                        <li>Tomate</li>
+                        <li v-for="(opcional, index) in burger.opcionais" :key="index">
+                            {{ opcional }}
+                        </li>
                     </ul>
                 </div>
                 <div>
                         <select name="stauts" class="status">
-                            <option value="">Selecione</option>
+                            <option v-for="s in status" :key="s.id" value="s.tipo" :selected="burger.status == s.tipo">
+                                {{ s.tipo }}
+                            </option>
                         </select>
                     <button class="delete-btn">Cancelar</button>
                 </div>
@@ -37,6 +40,39 @@
 <script>
 export default {
     name: 'Dashboard',
+    data(){
+        return{
+            burgers: null,
+            burger_id: null,
+            status: [],
+        }
+    },
+    methods: {
+        async getPedidos(){
+            const req = await fetch("http://localhost:3000/burgers");
+
+            const data = await req.json();
+
+            this.burgers = data;
+
+            //resgatar status
+
+            this.getStatus();
+
+
+        },
+        async getStatus(){
+
+            const req = await fetch("http://localhost:3000/status");
+
+            const data = await req.json();
+
+            this.status = data 
+        }
+    },
+    mounted(){
+        this.getPedidos();
+    }
 }
 </script>
 
