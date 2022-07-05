684a942b916d55568a7ad9d0878e825407d6e242
Thiago Silva Andrade <92411755+Fulozz@users.noreply.github.com>
2022-07-05 17:48:45 -0300

    Dashboard table criada.

--- src/views/Pedidos.vue
+++ src/views/Pedidos.vue
@@ -1,16 +1,23 @@
 <template>
     <section>
     <div class="main-container">
-      <h1>Pedidos</h1>
-    </div>
+      <h1>Gerenciar pedidos:</h1>
+      <Dashboard />
+      </div>
     </section>
 </template>
 
 <script>
+import Dashboard from '@/components/Dashboard.vue'
 export default {
   name: 'Pedidos',
   components:{
+    Dashboard
     
   }
 }
 </script>
+
+<style scoped>
+
+</style>
