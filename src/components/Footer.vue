b7b3fa66829e90de1c6b1465ea5aa630ed280672
Thiago Silva Andrade <92411755+Fulozz@users.noreply.github.com>
2022-07-04 21:05:21 -0300

    Vue Router modified, e ADD the footer and NavBar

---
+++ src/components/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+    <section>
+        <p>Footer</p>
+    </section>
+</template>
+<script>
+export default {
+    name: "Footer"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Pedidos.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="Pedidos">
+    <h1>Pedidos</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Pedidos',
+  components:{
+    
+  }
+}
+</script>
